2e0afdd7665a1804f74a2d9a03e5952de76ccdbc
鎌田由美子 <kamatayumiko@mei2024.local>
2025-03-26 10:41:27 +0900

    フラッシュメッセージの設定

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,15 +7,16 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_to root_path
+      redirect_to root_path, success: "ログインしました"
     else
-      render :new
+      flash.now[:danger] = "ログインに失敗しました"
+      render :new, status: :unprocessable_entity
     end
   end
 
 
   def destroy
     logout
-    redirect_to root_path, status: :see_other
+    redirect_to root_path, status: :see_other, success: "ログアウトしました"
   end
 end
